bc0d7ba3628377d2dd2754cda5857cb815834ca0
Sergio Regalado Alessi <serdigital@live.com.ar>
2025-07-30 23:00:55 -0300

    Add Class Nº38

---
+++ clase_38/codeando/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import User from "./components/User/User";
+
+const App = () => {
+    return (
+        <div className="app">
+            <User/>
+        </div>
+    );
+};
+
+export default App;
